92d1c99b12266d39cfc88b295af8e38428d852a1
KC Delmo <kcdelmo489@gmail.com>
2026-05-08 08:14:50 +0800

    Include environment files for deployment

---
+++ client-side/src/environments/environment.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDfX6iUPJE2lFUbEDElEQychLifi0oEmUg",
+    authDomain: "delmo-bd4bd.firebaseapp.com",
+    projectId: "delmo-bd4bd",
+    storageBucket: "delmo-bd4bd.appspot.com",
+    messagingSenderId: "899554194973",
+    appId: "1:899554194973:web:e9d283a52bb2c6cac878f4",
+    measurementId: "G-0XZ10EENTG"
+  }, // <--- Check mo ito, dapat comma lang, hindi };
+  
+  supabase: {
+    url: "https://ycuqiachyxjcizfkttfi.supabase.co",
+    key: "sb_publishable_hNQIz7LRM0Ik0mXA22a3jw_pb7J7Nat"
+  }
+}; // <--- Dito dapat ang huling saradong bracket.
